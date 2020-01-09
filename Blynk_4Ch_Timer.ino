bc2dba369196c610fb1f8e3a7f291b12340411b7
Bunnavit Sawangpiriyakij <smazcontrol@gmail.com>
2020-01-10 00:09:11 +0700

    Update Blynk_4Ch_Timer.ino

--- Blynk_4Ch_Timer.ino
+++ Blynk_4Ch_Timer.ino
@@ -19,12 +19,12 @@ WidgetRTC rtc;
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "f2b32463578a4a95811cda91c6487038";
+char auth[] = "Your Blynk Token";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "phloenlom_2.4GHz";
-char pass[] = "248248248";
+char ssid[] = "Your WiFi SSID";
+char pass[] = "Your WiFi Password";
 
 int count = 0;
 bool Connected2Blynk = false;
